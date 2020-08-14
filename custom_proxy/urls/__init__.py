51da202f819e8a16a6cef30a430f97287f1e9b59
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-14 09:35:58 +0900

    fix url module

---
+++ custom_proxy/urls/__init__.py
@@ -0,0 +1,29 @@
+import importlib
+from custom_proxy.page_types import PageType
+
+class UrlFactory:
+  def __init__(self, version, page_type, args):
+    self._module = importlib.import_module("custom_proxy.urls.v{}".format(int(version)))
+    self._page_type = page_type
+    self._args = args
+
+  def create(self):
+    return getattr(self._module, self._get_class_name())(**self._args)
+
+  def _get_class_name(self):
+    if self._page_type is PageType.RACER_PROFILE_PAGE:
+      return 'RacerProfilePageUrl'
+    elif self._page_type is PageType.EVENT_SCHEDULE_PAGE:
+      return 'EventSchedulePageUrl'
+    elif self._page_type is PageType.EVENT_ENTRIES_PAGE:
+      return 'EventEntriesPageUrl'
+    elif self._page_type is PageType.RACE_INFORMATION_PAGE:
+      return 'RaceInformationPageUrl'
+    elif self._page_type is PageType.RACE_RESULT_PAGE:
+      return 'RaceResultPageUrl'
+    elif self._page_type is PageType.RACE_EXHIBITION_INFORMATION_PAGE:
+      return 'RaceExhibitionInformationPageUrl'
+    elif self._page_type is PageType.RACE_ODDS_PAGE:
+      raise NotImplemented
+    else:
+      raise ValueError("unknown page type specified")
---
+++ tests/urls/test_urls.py
@@ -0,0 +1,46 @@
+from custom_proxy.versions import Version
+from custom_proxy.page_types import PageType
+from custom_proxy.urls import UrlFactory
+
+def test_to_create_race_profile_page_url():
+  args = { 'registration_number': 4444 }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.RACER_PROFILE_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/data/racersearch/profile?toban=4444"
+
+def test_to_create_monthly_schedule_page_url():
+  args = { 'year': 2020, 'month': 3 }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.EVENT_SCHEDULE_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/monthlyschedule?ym=202003"
+
+def test_to_create_daily_schedule_page_url():
+  args = { 'date': '2020-03-20' }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.EVENT_SCHEDULE_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/index?hd=20200320"
+
+def test_to_create_event_entries_page_url():
+  stadium_tel_code = 4
+  args = { 'stadium_tel_code': stadium_tel_code, 'event_starts_on': '2020-03-17' }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.EVENT_ENTRIES_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/rankingmotor?jcd=04&hd=20200317"
+
+def test_to_create_race_information_page_url():
+  stadium_tel_code = 4
+  race_number = 6
+  args = { 'stadium_tel_code': stadium_tel_code, 'race_opened_on': '2020-03-20', 'race_number': race_number }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.RACE_INFORMATION_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/racelist?rno=6&jcd=04&hd=20200320"
+
+def test_to_create_race_result_page_url():
+  stadium_tel_code = 4
+  race_number = 6
+  args = { 'stadium_tel_code': stadium_tel_code, 'race_opened_on': '2020-03-20', 'race_number': race_number }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.RACE_RESULT_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/raceresult?rno=6&jcd=04&hd=20200320"
+
+def test_to_create_race_exhibition_page_url():
+  stadium_tel_code = 4
+  race_number = 6
+  args = { 'stadium_tel_code': stadium_tel_code, 'race_opened_on': '2020-03-20', 'race_number': race_number }
+  factory = UrlFactory(version=Version.V1707, page_type=PageType.RACE_EXHIBITION_INFORMATION_PAGE, args=args)
+  assert str(factory.create()) == "https://boatrace.jp/owpc/pc/race/beforeinfo?rno=6&jcd=04&hd=20200320"
+  
